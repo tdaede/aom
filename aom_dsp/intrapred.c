44699ef4c8f3ac248c7739700d8924bb4c1b4cb0
Thomas Daede <tdaede@mozilla.com>
2017-09-24 08:54:25 -0700

    Changes to allow rav1e to link.
    Change-Id: I2704df39e88402bf5f78b2018ca4c4f8ad821407

--- aom_dsp/intrapred.c
+++ aom_dsp/intrapred.c
@@ -739,7 +739,7 @@ static INLINE void highbd_v_predictor(uint16_t *dst, ptrdiff_t stride, int bw,
   }
 }
 
-static INLINE void highbd_h_predictor(uint16_t *dst, ptrdiff_t stride, int bw,
+void highbd_h_predictor(uint16_t *dst, ptrdiff_t stride, int bw,
                                       int bh, const uint16_t *above,
                                       const uint16_t *left, int bd) {
   int r;
@@ -958,7 +958,7 @@ static INLINE void highbd_dc_128_predictor(uint16_t *dst, ptrdiff_t stride,
   }
 }
 
-static INLINE void highbd_dc_left_predictor(uint16_t *dst, ptrdiff_t stride,
+void highbd_dc_left_predictor(uint16_t *dst, ptrdiff_t stride,
                                             int bw, int bh,
                                             const uint16_t *above,
                                             const uint16_t *left, int bd) {
@@ -975,7 +975,7 @@ static INLINE void highbd_dc_left_predictor(uint16_t *dst, ptrdiff_t stride,
   }
 }
 
-static INLINE void highbd_dc_top_predictor(uint16_t *dst, ptrdiff_t stride,
+void highbd_dc_top_predictor(uint16_t *dst, ptrdiff_t stride,
                                            int bw, int bh,
                                            const uint16_t *above,
                                            const uint16_t *left, int bd) {
@@ -992,7 +992,7 @@ static INLINE void highbd_dc_top_predictor(uint16_t *dst, ptrdiff_t stride,
   }
 }
 
-static INLINE void highbd_dc_predictor(uint16_t *dst, ptrdiff_t stride, int bw,
+void highbd_dc_predictor(uint16_t *dst, ptrdiff_t stride, int bw,
                                        int bh, const uint16_t *above,
                                        const uint16_t *left, int bd) {
   int i, r, expected_dc, sum = 0;
